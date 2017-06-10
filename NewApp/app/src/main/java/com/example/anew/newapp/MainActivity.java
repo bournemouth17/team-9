bd9ff7496ffb07b468b01faabdabab7db36eb275
Devashish <deva_0801@hotmail.com>
2017-06-10 06:57:50 +0100

    appdata
    Main app for volunteers

---
+++ NewApp/app/src/main/java/com/example/anew/newapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.anew.newapp;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.anew.newapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        //findViewById(R.id.button);
+        startActivity(intent);
+    }
+
+
+}
